7e475b09328f0d8526d02cc81e004058f469f9ee
Leonid <lion6149@mail.ru>
2021-08-08 15:45:33 +0300

    Removed abs values and added readme #1

--- Commands/export.bat
+++ Commands/export.bat
@@ -1,8 +1,8 @@
-set FlashPath="C:\Program Files (x86)\Macromedia\Flash 8\Flash.exe"
+set FlashPath="%FlashDir%\Flash.exe"
 set ExportScriptPath="%~dp0\Export.jsfl"
 set ExportScriptParams="%~dp0\ExportParams.txt"
 @echo off
 echo %1 %2> %ExportScriptParams%
 %FlashPath% %ExportScriptPath%
 
-"D:\progs\ruffle\ruffle.exe" %2
+"%RuffleDir%\ruffle.exe" %2
