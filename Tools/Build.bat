b9d154597a49228015129a69c0f8aabdbc5b043d
Leonid <lion6149@mail.ru>
2021-08-14 20:46:46 +0300

    Fixed build and run via tmp in C:/../Commands #1

--- Tools/Build.bat
+++ Tools/Build.bat
@@ -1,12 +1,10 @@
 set FlashPath=%1
-set RufflePath=%2
+set ExportScriptParams=%2
 set FlaPath=%3
 set SwfPath=%4
 set ExportScriptPath="%~dp0\Export.jsfl"
-set ExportScriptParams="%~dp0\ExportParams.txt"
 del %ExportScriptParams%
 del %SwfPath%
 @echo off
 echo %FlaPath% %SwfPath%> %ExportScriptParams%
 %FlashPath% %ExportScriptPath%
-%RufflePath% %SwfPath%
